7db0fd05edaab2dbb8bd1e51b7efbff61d4346c1
CK151 <andersonoki15@gmail.com>
2022-11-29 14:26:17 +0300

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+//https://api.chucknorris.io/jokes/random
+
+
+const joke = document.querySelector(".joke");
+const btn = document.querySelector(".button");
+
+
+const url = "https://api.chucknorris.io/jokes/random";
+
+
+
+btn.addEventListener("click", getJoke);
+
+/*function getJoke() {
+    fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        joke.innerHTML = data.value;
+    })
+}*/
+
+
+async function getJoke() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        //console.log(data);
+        joke.innerHTML = data.value;
+    }
+    catch (error){
+        joke.innerHTML = error.message;
+    }
+    
+}
